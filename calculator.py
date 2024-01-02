ea1327ff0a13acb43d86f431d2100fd592cb1fa1
jasmintajshaik <shaikjasmintaj26@gmail.com>
2024-01-02 20:10:34 +0530

    Committing files

---
+++ calculator.py
@@ -0,0 +1,15 @@
+num1 = 10
+num2 = 5
+def add():
+    return(num1+num2)
+def sub():
+    print(num1-num2)
+def mul():
+    print(num1*num2)
+def div():
+    print(num1/num2)
+
+print(add())
+sub()
+mul()
+div()
